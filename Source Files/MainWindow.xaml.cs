c0c0dc25d874eead1906d57718878daedca04997
Huseyin Avni Sarikaya <hasarikaya00@gmail.com>
2022-03-26 20:15:32 +0300

    Time parameter added
    Time parameter of the 3D print added to textbox section

--- Source Files/MainWindow.xaml.cs
+++ Source Files/MainWindow.xaml.cs
@@ -14,9 +14,11 @@ namespace WpfApp2
         private string grnm = "";
         private string isnm = "";
         private string flnm = "";
+        private string sure = "";
         double isint;
         double grint;
         double flint;
+        double surent;
 
 
         public MainWindow()
@@ -25,6 +27,7 @@ namespace WpfApp2
             gram.Text = "0";
             ss.Text = "0";
             fl.Text = "0";
+            sr.Text = "0";
         }
 
         private void bt_Click(object sender, RoutedEventArgs e)
@@ -37,9 +40,11 @@ namespace WpfApp2
                 grnm = gram.Text;
                 isnm = ss.Text;
                 flnm = fl.Text;
+                sure = sr.Text;
                 isint = double.Parse(isnm.ToString());
                 grint = double.Parse(grnm.ToString());
                 flint = double.Parse(flnm.ToString());
+                surent = double.Parse(sure.ToString());
 
             }
             catch (System.FormatException)
@@ -49,7 +54,7 @@ namespace WpfApp2
 
             }
 
-            double net = (flint / 1000) * grint + 1 + isint;
+            double net = (flint / 1000) * grint + isint + (surent * 0.36);
             double kar = (net * 30 / 100) + net;
             lb.Text = $"Net fiyat:{net}";
             nm.Text = $"Kar ile birlikte fiyat:{kar}";
